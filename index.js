83de3e7a6f4e672a2321500e2c73d8cb8d25ab80
Yasith Wimukthi <yasith@imperiallearning.co.uk>
2023-01-09 13:31:53 +0530

    add styles

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
 
-
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
@@ -12,19 +11,35 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html');
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/public/swift.html');
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/css.html');
+});
+
 // tech namespace
 const tech = io.of('/tech');
 
 tech.on('connection', (socket) => {
-    console.log('user connected');
-    socket.on('message', (msg) => {
-        console.log(`message: ${msg}`);
-        io.emit('message', msg);
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
+    })
+
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
 
-        io.emit('message', 'user disconnected');
+        tech.emit('message', 'user disconnected');
     })
 })
